164f92f2b64b19e04365cf8b08ef72973b6824b1
Norihiro Kamae <norihiro@nagater.net>
2023-11-15 23:04:55 +0900

    CI(windows): Archive deps directory

--- .github/scripts/Package-Windows.ps1
+++ .github/scripts/Package-Windows.ps1
@@ -60,7 +60,7 @@ function Package {
 
     Log-Group "Archiving ${ProductName}..."
     $CompressArgs = @{
-        Path = (Get-ChildItem -Path "${ProjectRoot}/release/${Configuration}" -Exclude "${OutputName}*.*")
+        Path = "${ProjectRoot}/.deps/"
         CompressionLevel = 'Optimal'
         DestinationPath = "${ProjectRoot}/release/${OutputName}.zip"
         Verbose = ($Env:CI -ne $null)
